a239b503645f0b099aa697480549bc1a27962db9
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-03-05 13:35:21 -0300

    Adicionar categorias faltantes
    Os ids foram modificados pois eu utilizarei o end-point /search em vez
    de /videos, ou seja, não utilizarei os categoryId's da google.

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,7 +1,10 @@
 export default {
-  "24": { id: 24, title: "Entreterimento" },
-  "30": { id: 30, title: "Filmes" },
-  "20": { id: 20, title: "Jogos" },
-  "10": { id: 10, title: "Música" },
-  "43": { id: 43, title: "Seriados" }
+  "1": { id: 1, title: "Arte" },
+  "2": { id: 2, title: "Música" },
+  "3": { id: 3, title: "Entretenimento" },
+  "4": { id: 4, title: "Cultura" },
+  "5": { id: 5, title: "Filmes" },
+  "6": { id: 6, title: "Seriados" },
+  "7": { id: 7, title: "Jogos" },
+  "8": { id: 8, title: "Diversão" }
 };
