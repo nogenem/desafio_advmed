1af7f68ef7653e9151e68269b9120419797d1a6f
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-03-06 15:29:35 -0300

    Modificar a estrutura da Store
    Irei utilizar os 'videoIds' para pegar os videos mais facilmente na
    página de detalhes

--- src/reducers/videos.js
+++ src/reducers/videos.js
@@ -4,8 +4,8 @@ import { VIDEOS_BY_CATEGORY_FETCHED } from "../constants/action_types";
 
 /*
 state = {
-  "category_id": [
-    {
+  "category_id": {
+    "video_id": {
       id: "",
       url: "",
       title: "",
@@ -19,25 +19,30 @@ state = {
       dislikes: XXX (maybe?)
     },
     ...
-  ],
+  },
   ...
 }
 */
 export const INITIAL_STATE = {};
 
 const reshapeByCategoryData = (data, categoryId) => {
-  const result = data.items.map(({ id, snippet }) => ({
-    id: id.videoId,
-    url: `https://www.youtube.com/watch?v=${id.videoId}`,
-    title: snippet.title,
-    // o end-point '/search' retorna apenas a descrição
-    // com um numero máximo de caracteres e adiciona
-    // '...' após este numero máximo
-    short_description: snippet.description,
-    thumbnails: {
-      medium: snippet.thumbnails.medium.url
-    }
-  }));
+  const result = {};
+
+  data.items.forEach(({ id, snippet }) => {
+    result[id.videoId] = {
+      id: id.videoId,
+      url: `https://www.youtube.com/watch?v=${id.videoId}`,
+      title: snippet.title,
+      // o end-point '/search' retorna apenas a descrição
+      // com um numero máximo de caracteres e adiciona
+      // '...' após este numero máximo
+      short_description: snippet.description,
+      thumbnails: {
+        medium: snippet.thumbnails.medium.url
+      }
+    };
+  });
+
   return {
     [categoryId]: result
   };
@@ -64,5 +69,5 @@ const getCategoryId = (state, categoryId) => categoryId;
 export const getByCategoryId = createSelector(
   getVideosHash,
   getCategoryId,
-  (videosHash, categoryId) => videosHash[categoryId] || []
+  (videosHash, categoryId) => Object.values(videosHash[categoryId] || [])
 );
