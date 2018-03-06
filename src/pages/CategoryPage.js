b1d4e53c368ce8dc8b763913526e28ddf20e1fd8
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-03-06 09:46:18 -0300

    Adicionar o campo de filtro dos resultados

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Container, Row, Media } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  FormGroup,
+  Label,
+  Input,
+  Media
+} from "reactstrap";
 
 import "./CategoryPage.css";
 import { fetchByCategoryId } from "../actions/videos";
@@ -14,7 +22,8 @@ class CategoryPage extends Component {
 
     this.state = {
       loading: false,
-      videos: props.videos
+      videos: props.videos,
+      filter: ""
     };
   }
 
@@ -35,6 +44,22 @@ class CategoryPage extends Component {
     }
   };
 
+  onFilterChange = e => {
+    const { value } = e.target;
+    if (value === "") {
+      this.setState({
+        filter: "",
+        videos: this.props.videos
+      });
+      return;
+    }
+    const filteredVideos = this.props.videos.filter(video => {
+      const txt = video.title + video.short_description;
+      return txt.toLowerCase().includes(value.toLowerCase());
+    });
+    this.setState({ filter: e.target.value, videos: filteredVideos });
+  };
+
   loadData = props => {
     const { categoryId } = props.match.params;
     this.setState({ loading: true });
@@ -52,12 +77,27 @@ class CategoryPage extends Component {
   };
 
   render() {
-    if (this.state.loading) return <div>Loading...</div>;
+    const { loading, videos, filter } = this.state;
+    if (loading) return <div>Loading...</div>;
     return (
       <Container className="mt-3">
+        <FormGroup row className="offset-sm-1">
+          <Label for="filter-input" sm={2}>
+            Filtro:
+          </Label>
+          <Col sm={8}>
+            <Input
+              type="text"
+              name="filter"
+              id="filter-input"
+              value={filter}
+              onChange={this.onFilterChange}
+            />
+          </Col>
+        </FormGroup>
         <Row>
           <Media list className="list-unstyled">
-            {this.state.videos.map(video => (
+            {videos.map(video => (
               <VideoListItem key={video.id} video={video} />
             ))}
           </Media>
