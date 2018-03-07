8c0523f11137227b42a0b1ca835df5143a6593f1
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-03-07 11:07:42 -0300

    Adicionar pequena proteção

--- src/utils/handleErrors.js
+++ src/utils/handleErrors.js
@@ -1,7 +1,9 @@
 const handleErrors = err => {
-  const errData = err.response.data.error;
-  if (errData.code === 403 && errData.errors[0].domain === "usageLimits")
-    return "Excedido o limite diário da API do youtube.";
+  if (err.response && err.response.data) {
+    const errData = err.response.data.error;
+    if (errData.code === 403 && errData.errors[0].domain === "usageLimits")
+      return "Excedido o limite diário da API do youtube.";
+  }
   return "Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.";
 };
 
